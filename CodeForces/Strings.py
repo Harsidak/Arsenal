2341ebe7c80134238e1eb0360cad5be204fa0d2b
Harsidak <harsidak.singh.banwait.13@gmail.com>
2026-05-11 21:14:30 +0530

    Organised the codeforces directory

--- CodeForces/Strings.py
+++
@@ -1,9 +0,0 @@
-str_1 = input().strip().lower()
-str_2 = input().strip().lower()
-
-if str_1 < str_2:
-    print(-1)
-elif str_1 > str_2:
-    print(1)
-else:    
-    print(0)
--- CodeForces/presents.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-presents = list(map(int, input().split()))
-result = [0] * n
-for i in range(n):
-    result[presents[i] - 1] = i + 1
-print(*result)
