ce7a0d7399030379e15dc077af71a78ab8b1ef0b
Harsidak <harsidak.singh.banwait.13@gmail.com>
2026-07-28 21:32:00 +0530

    feat: add CodeForces solution 706B and implement FrenchDesk simulation in Python

--- CodeForces/SOLVED/706B.c
+++ CodeForces/SOLVED/706B.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int Bs(int *array, int target){
-    int len = sizeof(array) / sizeof(array[0]);
+int Bs(int *array,int len, int target){
     int low = 0;
     int high = len - 1;
-    int ans = -1;
-
+    int ans = len;
     while(low <= high){
         int mid = low + (high - low) / 2;
-        if(array[mid] >= target){
+        if(array[mid] > target){
             ans = mid;
             high = mid - 1;
         }else{
             low = mid + 1;
         }
     }
-        return ans;
+    return ans;
 }
+
+int compare(const void *a, const void *b){
+    return *(int*)a - *(int*)b;
+}
+
 int main(){
     int n;
     scanf("%d", &n);
-    int arr[n + 1];
+
+    int arr[n];
     for(int i = 0; i < n; i++){
         scanf("%d", &arr[i]);
     }
@@ -30,12 +34,12 @@ int main(){
 
     int q;
     scanf("%d", &q);
-    int arr2[q + 1];
-    for(int i = 0; i < q; i++){
-        scanf("%d", &arr2[i]);
 
-        int count = Bs(arr, arr2[i]);
-        printf("%d\n", count);
+    for(int i = 0; i < q; i++){
+        int x;
+        scanf("%d", &x);
+
+        printf("%d\n",Bs(arr,n,x));
     }
 
     return 0;
