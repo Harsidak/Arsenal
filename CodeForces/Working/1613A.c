ee24382cb19f2a4e4dd34aa228cd89a517469f63
Harsidak <harsidak.singh.banwait.13@gmail.com>
2026-08-18 09:33:54 +0530

    solved 1613C

--- CodeForces/Working/1613A.c
+++ CodeForces/Working/1613A.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int bs(int arr, int length, int target){
-    
+int isValid(int num, long long int arr[], long long int k, long long int h){
+
+    long long int damage = 0;
+
+    for(int i = 0; i < num - 1; i++){
+        long long int gap = arr[i + 1] - arr[i];
+
+        if (gap <= k){
+            damage += gap;
+        }else{
+            damage += k;
+        }
+
+        if (damage >= h){
+            return 1;
+        }
+    }
+    damage += k;
+    return damage >= h ? 1 : 0;
+}
+
+long long int bs(long long int arr[], long long int length, long long int h){
+    long long int low = 1;
+    long long int high = h;
+    long long int ans = -1;
+
+    while(low <= high){
+        long long int mid = low + (high - low) / 2;
+
+        if(isValid(length, arr, mid, h)){
+            ans = mid;
+            high = mid - 1;
+        }else{
+            low = mid + 1;
+        }
+    }
+
+    return ans;
 }
 
 int main(){
@@ -10,11 +46,12 @@ int main(){
     while(t--){
         int n;
         long long int h;
-        scanf("%d %d",&n,&h);
+        scanf("%d %lld",&n,&h);
 
         long long int arr[n];
         for(int i = 0;i < n;i++){scanf("%lld", &arr[i]);}
 
+        printf("%lld\n", bs(arr, n, h));
     }
     return 0;
 }
